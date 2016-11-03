f39b658b5682c8e3e16c0e28d92adb6a458846b2
Agne <agne240@me.com>
2016-11-03 14:59:15 +0100

    Location service for MapsActivity

--- app/src/main/java/no/enkeloversikt/newroute/new_route/MapsActivity.java
+++ app/src/main/java/no/enkeloversikt/newroute/new_route/MapsActivity.java
@@ -1,18 +1,37 @@
 package no.enkeloversikt.newroute.new_route;
 
-import android.support.v4.app.FragmentActivity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    public double longitude, latitude;
+
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    private ProgressDialog apiProgress;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    private boolean customLocation = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +41,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        apiProgress = new ProgressDialog(this);
+        apiProgress.setMessage("loading...");
+        apiProgress.show();
+
+        requestFineLocationPermit();
+
     }
 
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Permission request for Android 6.0 and later
+     */
+    public void requestFineLocationPermit() {
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            android.Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            requestLoc();
+        }
+    }
+
+    /**
+     * Permission request for Android 6.0 and later
+     *
+     * @param requestCode  int
+     * @param permissions  string array
+     * @param grantResults int array
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // Register the listener with the Location Manager to receive location updates
+                    requestLoc();
+
+                } else {
+                    requestFineLocationPermit();
+                }
+            }
+        }
+    }
+
+    /**
+     * request location updates
+     */
+    public void requestLoc() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        //location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        //setLatLng(location);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                if (!customLocation) {
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+
+                    LatLng current = new LatLng(latitude, longitude);
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current, 12));
+                }
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // 6000mm == 6s
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 6000, locationListener);
+
+    }
+
+
+    /**
+     * Google maps init
+     *
+     * @param googleMap mMap
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
+
 }
